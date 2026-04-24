d69e0e0d758c4206d83bd9d1cca68e2b9cf35069
simatosc <costas.simatos@sigmacubed.eu>
2026-04-24 11:03:03 +0200

    Adapted annotation packages

--- xmlvalidator-common/src/main/java/eu/europa/ec/itb/xml/validation/SchematronURIResolver.java
+++ xmlvalidator-common/src/main/java/eu/europa/ec/itb/xml/validation/SchematronURIResolver.java
@@ -20,7 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamSource;
